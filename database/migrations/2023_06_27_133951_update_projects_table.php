9bdff21591045e26187ba346e8e1481d530477b1
Sara Gisondi <sara.gisondi@outlook.it>
2023-06-27 15:48:53 +0200

    rollback foreign key

--- database/migrations/2023_06_27_133951_update_projects_table.php
+++ database/migrations/2023_06_27_133951_update_projects_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('projects', function (Blueprint $table) {
-            $table->unsignedBigInteger('type_id')->nullable();
+            $table->unsignedBigInteger('type_id')->nullable()->after('id');
             $table->foreign('type_id')
                     ->references('id')
                     ->on('types')
@@ -30,7 +30,8 @@ return new class extends Migration
     public function down()
     {
         Schema::table('projects', function (Blueprint $table) {
-            //
+            $table->dropForeign(['type_id']);
+            $table->dropColumn('type_id');
         });
     }
 };
